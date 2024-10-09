52935154e3f3150103e7b4a69db740a7d9913b2b
AleKolar <gefest-173@rambler.ru>
2024-10-09 23:09:39 +0300

    data_env

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declared_attr
+from config import settings
+
+DATABASE_URL = (
+    f'postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
+)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+class Base:
+    @declared_attr
+    def __tablename__(cls):
+        return f"{cls.__name__.lower()}s"
+
+
